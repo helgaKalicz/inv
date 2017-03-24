48d44f9fc9b5389c7b963970a8af81b57c6845d2
helgaKalicz <kalicz.helga@gmail.com>
2017-03-24 21:36:39 +0100

    first 3 steps

--- gameInventory.py
+++ gameInventory.py
@@ -1,14 +1,27 @@
 # This is the file where you must work. Write code in the functions, create new functions, 
 # so they work according to the specification
 
+inventory = {'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12}
+added_items = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
+
 # Displays the inventory.
 def display_inventory(inventory):
-    pass
+    print("Inventory:")
+    for i, j in inventory.items():
+        print(j, i)
+    print("Total number of items:", sum(inventory.values()))
+display_inventory(inventory)
 
 
 # Adds to the inventory dictionary a list of items from added_items.
 def add_to_inventory(inventory, added_items):
-    pass
+    for i in range(len(added_items)):
+        if (added_items[i]) in inventory:
+            inventory[added_items[i]] += 1
+        else:
+            inventory[added_items[i]] = 1
+    display_inventory(inventory)
+add_to_inventory(inventory, added_items)
 
 
 # Takes your inventory and displays it in a well-organized table with 
@@ -19,7 +32,27 @@ def add_to_inventory(inventory, added_items):
 #   in descending order
 # - "count,asc" means the table is ordered by count in ascending order
 def print_table(inventory, order=None):
-    pass
+    import operator
+    inv = list(reversed(sorted(inventory.items(), key=operator.itemgetter(1))))
+    a = []
+    b = []
+    for i in range(len(inv)):
+        a.append(inv[i][0])
+        b.append(str(inv[i][1]))
+    a_lenght = max([len(a[i]) for i in range(len(a))])
+    b_lenght = max([len(b[i]) for i in range(len(b))])
+    if a_lenght < len("item name"):
+        a_lenght = len("item name")
+    if b_lenght < len("count"):
+        b_lenght = len("count")
+    print("Inventory:")
+    print("-" * (b_lenght + a_lenght + 2))
+    print(" " * (b_lenght - len("count")) + "count" + " " * (2 + a_lenght - len("item name")) + "item name")
+    for i in range(len(a)):
+        print(" " * (b_lenght - len(str(b[i]))) + str(b[i]) + " " * (2 + a_lenght - len(a[i])) + a[i])
+    print("-" * (b_lenght + a_lenght + 2))
+    print("Total number of items:", sum(inventory.values()))
+print_table(inventory, order=None)
 
 
 # Imports new inventory items from a file
