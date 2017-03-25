31f791897709c67063f5423f169422ca9f3ea6e5
helgaKalicz <kalicz.helga@gmail.com>
2017-03-25 16:31:27 +0100

    Seems to work and it is colourful

--- gameInventory.py
+++ gameInventory.py
@@ -3,13 +3,19 @@
 
 inventory = {}
 added_items = []
+# Making some solours
+c0 = '\033[0m'
+c1 = '\033[34m'
+c2 = '\033[35m'
+c3 = '\033[94m'
+c4 = '\033[95m'
 
 # Displays the inventory.
 def display_inventory(inventory):
-    print("\nInventory:")
+    print(c2 + "\nInventory:")
     for i, j in inventory.items():
-        print(j, i)
-    print("Total number of items:", sum(inventory.values()), "\n")
+        print(c3 + str(j), i)
+    print(c2 + "Total number of items:" + c1, sum(inventory.values()), c0 + "\n")
 
 
 # Adds to the inventory dictionary a list of items from added_items.
@@ -30,6 +36,7 @@ def add_to_inventory(inventory, added_items):
 #   in descending order
 # - "count,asc" means the table is ordered by count in ascending order
 def print_table(inventory, order=None):
+    # Making two list from a sorted inventory.
     import operator
     inv = list(reversed(sorted(inventory.items(), key=operator.itemgetter(1))))
     a = []
@@ -37,12 +44,12 @@ def print_table(inventory, order=None):
     for i in range(len(inv)):
         a.append(inv[i][0])
         b.append(str(inv[i][1]))
-    # Counts the strings lenght in a list.
+    # Counts the strings' lenght in the lists, and making new lists of them.
     a1 = [len(a[i]) for i in range(len(a))]
     b1 = [len(b[i]) for i in range(len(b))]
     a2 = "item name"
     b2 = "count"
-    # This code is for in case of an empty inventory.
+    # This code is for in case of an empty inventory, and also to handle any lenght.
     if a1 and (max(a1) >= len(a2)):
         a_lenght = max(a1)
     else:
@@ -52,13 +59,13 @@ def print_table(inventory, order=None):
     else:
         b_lenght = len(b2)
     # Printing
-    print("Inventory:")
-    print(" " * (b_lenght - len(b2)) + b2 + " " * (2 + a_lenght - len(a2)) + a2)
-    print("-" * (b_lenght + a_lenght + 2))
+    print(c2 + "\nInventory:")
+    print(c1 + " " * (b_lenght - len(b2)) + b2 + " " * (2 + a_lenght - len(a2)) + a2)
+    print(c2 + "-" * (b_lenght + a_lenght + 2))
     for i in range(len(a)):
-        print(" " * (b_lenght - len(str(b[i]))) + str(b[i]) + " " * (2 + a_lenght - len(a[i])) + a[i])
-    print("-" * (b_lenght + a_lenght + 2))
-    print("Total number of items:", sum(inventory.values()), "\n")
+        print(c3 + " " * (b_lenght - len(str(b[i]))) + str(b[i]) + " " * (2 + a_lenght - len(a[i])) + a[i])
+    print(c2 + "-" * (b_lenght + a_lenght + 2))
+    print(c2 + "Total number of items:" + c1, sum(inventory.values()), c0 + "\n")
 
 
 # Imports new inventory items from a fileprint(inventory
@@ -67,9 +74,8 @@ def print_table(inventory, order=None):
 # The file format is plain text with comma separated values (CSV).
 def import_inventory(inventory, filename="import_inventory.csv"):
     with open(filename, "r") as f:
-        loot = list(f.read().split(','))
-    add_to_inventory(inventory, loot)
-    print_table(inventory, order=None)
+        added_items = list(f.read().split(','))
+    add_to_inventory(inventory, added_items)
 
 
 # Exports the inventory into a .csv file.
@@ -92,4 +98,4 @@ def export_inventory(inventory, filename="export_inventory.csv"):
         for i in range(len(loot)-1):
             f.write((loot[i] + ","))
         f.write(loot[len(loot)-1])
-print_table(inventory, order=None)
+    print_table(inventory, order=None)
